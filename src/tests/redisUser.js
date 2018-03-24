0a775ca94446b226b37df695f440f21481d0ea2c
Michael Angerman <stormasm@gmail.com>
2018-03-24 13:55:11 -0700

    async await mocha chai redis testing is WORKING !!

--- src/tests/redisUser.js
+++ src/tests/redisUser.js
@@ -1,16 +1,15 @@
-
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { readLocation, readName } from '../redis/readUtils';
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { locationMember, readLocation, readName } from "../redis/readUtils";
 import { writeLocation, writeName } from "../redis/writeUtils";
 
-describe('Star Wars Query Tests', () => {
-  describe('Basic Queries', () => {
-    it('Write location to redis', async () => {
-      writeLocation('stormasm', 'corvallis');
-      writeName('stormasm', 'michael angerman');
-      locationResult = locationMember('corvallis','stormasm');
-      expect(locationResult).to.deep.equal(1);
-      });
+describe("Star Wars Query Tests", () => {
+  describe("Basic Queries", () => {
+    it("Write location to redis", async () => {
+      writeLocation("stormasm", "corvallis");
+      writeName("stormasm", "michael angerman");
+      const result = await locationMember("corvallis", "stormasm");
+      expect(result).to.deep.equal(1);
     });
   });
+});
