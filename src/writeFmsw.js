aab02b7c5a7b87f517ee54e246714132d9f8e45e
Michael Angerman <stormasm@gmail.com>
2018-03-26 14:14:59 -0700

    starting to work on repository forks, stars, watchers, and mentionableUsers

---
+++ src/writeFmsw.js
@@ -0,0 +1,212 @@
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "node-fetch";
+import gql from "graphql-tag";
+import * as fs from "fs";
+
+import { writeLocation, writeName } from "./redis/writeUtils";
+
+async function getJsonKeyFromFile(filename) {
+  var r1 = await readJsonDataFromFilename(filename, "utf8");
+  var r2 = r1.trim();
+  var r3 = JSON.parse(r2);
+  var r4 = r3.key;
+  return r4;
+}
+
+async function readJsonDataFromFilename(fileName, type) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, type, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+async function getClient(githubApiKey) {
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: "https://api.github.com/graphql",
+      headers: {
+        Authorization: `Bearer ${githubApiKey}`
+      },
+      fetch
+    }),
+    cache: new InMemoryCache()
+  });
+  return client;
+}
+
+const repositoryMentionableUsers = gql`
+  query MentionableUsers($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+       name
+       nameWithOwner
+       mentionableUsers(first: 100) {
+         totalCount
+         edges {
+           cursor
+           node {
+             login
+           }
+         }
+       }
+     }
+  }
+`;
+
+const repositoryMentionableUsersWithCursor = gql`
+  query MentionableUsers($owner: String!, $name: String!, $before: String) {
+    repository(owner: $owner, name: $name) {
+       name
+       nameWithOwner
+       mentionableUsers(first: 100, before: $before) {
+         totalCount
+         edges {
+           cursor
+           node {
+             login
+           }
+         }
+       }
+     }
+  }
+`;
+
+
+
+
+
+
+const locationQueryWithCursor = gql`
+  query LocationSearchCursor($location: String!, $before: String) {
+    search(query: $location, type: USER, first: 100, before: $before) {
+      userCount
+      edges {
+        cursor
+        node {
+          ... on User {
+            name
+            login
+            location
+            __typename
+          }
+          ... on Organization {
+            name
+            login
+            location
+            __typename
+          }
+        }
+      }
+    }
+  }
+`;
+
+// const options = { before: "Y3Vyc29yOjb" };
+
+async function getInitialGithubData(client, city) {
+  const options = { location: `location:${city}` };
+  return new Promise((resolve, reject) => {
+    client
+      .query({
+        query: locationQuery,
+        variables: options
+      })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+async function getGithubData(client, options) {
+  return new Promise((resolve, reject) => {
+    client
+      .query({
+        query: locationQueryWithCursor,
+        variables: options
+      })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+async function handleRedis(githubData) {
+  return Promise.resolve(githubData)
+    .then(function(mike) {
+      return mike;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+async function iterateOverCursor(client, cursor, city) {
+  const options = { location: `location:${city}`, before: cursor };
+
+  let myjson = await getGithubData(client, options);
+  let myredis = await handleRedis(myjson);
+  await getCursorFromData(client, myredis, city);
+}
+
+async function getCursorFromData(client, value, city) {
+  let userCount = value.data.search.userCount;
+  let edgeAry = value.data.search.edges;
+
+  processEdgeAry(edgeAry, city);
+
+  let edgeAryLength = edgeAry.length;
+  let cursor = edgeAry[edgeAryLength - 1].cursor;
+  console.log("userCount = ", userCount);
+  console.log("edgeAry length = ", edgeAryLength);
+  console.log("cursor = ", cursor);
+
+  if (edgeAryLength == 1) {
+    return 1;
+  }
+
+  iterateOverCursor(client, cursor, city);
+}
+
+function processEdgeAry(edgeAry, location) {
+  edgeAry.forEach(function(item) {
+    //
+    //  Leave this here for later when you want to do some filtering
+    //  based on regex matching to filter out for example
+    //  South Bend, Indiana from Bend, Oregon
+    //  For now South Bend, Indiana is getting in there
+    //  When I just put in Bend
+    //
+    //  let location = item.node.location;
+    //
+    let login = item.node.login;
+    let name = item.node.name;
+    writeLocation(login, location);
+    writeName(login, name);
+  });
+}
+
+async function goGql(city) {
+  let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
+  let client = await getClient(githubApiKey);
+  let myjson = await getInitialGithubData(client, city);
+  let myredis = await handleRedis(myjson);
+  await getCursorFromData(client, myredis, city);
+}
+
+const cities = ["corvallis", "bend", "eugene"];
+// const cities = ["corvallis"];
+
+cities.forEach(function(city) {
+  goGql(city);
+});
