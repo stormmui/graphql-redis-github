5701158ad5254aad2e8307364e596fc2a59bb1fa
Michael Angerman <stormasm@gmail.com>
2018-03-27 21:30:33 -0700

    writeUser is ported over and up and running !

--- src/location/writeUser.js
+++ src/location/writeUser.js
@@ -33,104 +33,6 @@ const query = gql`
   }
 `;
 
-// const options = { before: "Y3Vyc29yOjb" };
-/*
-async function getInitialGithubData(client, city) {
-  const options = { location: `location:${city}` };
-  return new Promise((resolve, reject) => {
-    client
-      .query({
-        query: locationQuery,
-        variables: options
-      })
-      .then(data => {
-        resolve(data);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-}
-
-async function getGithubData(client, options) {
-  return new Promise((resolve, reject) => {
-    client
-      .query({
-        query: locationQuery,
-        variables: options
-      })
-      .then(data => {
-        resolve(data);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-}
-
-async function iterateOverCursor(client, cursor, city) {
-  const options = { location: `location:${city}`, before: cursor };
-
-  let myjson = await getGithubData(client, options);
-  let myredis = await handlePromise(myjson);
-  await getCursorFromData(client, myredis, city);
-}
-
-async function getCursorFromData(client, value, city) {
-  let userCount = value.data.search.userCount;
-  let edgeAry = value.data.search.edges;
-
-  processEdgeAry(edgeAry, city);
-
-  let edgeAryLength = edgeAry.length;
-  let cursor = edgeAry[edgeAryLength - 1].cursor;
-  console.log("userCount = ", userCount);
-  console.log("edgeAry length = ", edgeAryLength);
-  console.log("cursor = ", cursor);
-
-  if (edgeAryLength == 1) {
-    return 1;
-  }
-
-  iterateOverCursor(client, cursor, city);
-}
-
-function processEdgeAry(edgeAry, location) {
-  edgeAry.forEach(function(item) {
-    //
-    //  Leave this here for later when you want to do some filtering
-    //  based on regex matching to filter out for example
-    //  South Bend, Indiana from Bend, Oregon
-    //  For now South Bend, Indiana is getting in there
-    //  When I just put in Bend
-    //
-    //  let location = item.node.location;
-    //
-    let login = item.node.login;
-    let name = item.node.name;
-    writeLocation(login, location);
-    writeName(login, name);
-  });
-}
-
-async function goGql(city) {
-  let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
-  let client = await getClient(githubApiKey);
-  let myjson = await getInitialGithubData(client, city);
-  let myredis = await handlePromise(myjson);
-  await getCursorFromData(client, myredis, city);
-}
-
-const cities = ["corvallis", "bend", "eugene"];
-// const cities = ["corvallis"];
-
-cities.forEach(function(city) {
-  goGql(city);
-});
-*/
-
-
-
 async function iterateOverCursor(client, cursor, options_in) {
   const options = {
     location: options_in.location,
@@ -144,7 +46,6 @@ async function iterateOverCursor(client, cursor, options_in) {
 }
 
 async function getCursorFromData(client, options, value) {
-
   let userCount = value.data.search.userCount;
   let edgeAry = value.data.search.edges;
 
@@ -163,7 +64,6 @@ async function getCursorFromData(client, options, value) {
   iterateOverCursor(client, cursor, options);
 }
 
-
 function processEdgeAry(edgeAry, location) {
   edgeAry.forEach(function(item) {
     //
@@ -181,17 +81,6 @@ function processEdgeAry(edgeAry, location) {
     writeName(login, name);
   });
 }
-/*
-function processEdgeAry(edgeAry, repository) {
-  edgeAry.forEach(function(item) {
-    let login = item.node.login;
-    // eventually we will rename this method
-    // but for now it sadd's a member to a set
-
-    writeLocation(login, repository);
-  });
-}
-*/
 
 async function goGql(options) {
   let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
