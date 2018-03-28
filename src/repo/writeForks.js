f3fac6a7d2d5507eb69fd0c23b3e3c8610f8e7eb
Michael Angerman <stormasm@gmail.com>
2018-03-27 20:54:17 -0700

    mw

--- src/repo/writeForks.js
+++ src/repo/writeForks.js
@@ -5,7 +5,7 @@ import {
   readJsonDataFromFilename
 } from "./../util/file-util";
 import { getClient } from "./../util/apollo-util";
-import { getInitialGithubData, getGithubData } from "./../util/github-util";
+import { getGithubData } from "./../util/github-util";
 import { handlePromise } from "./../util/promise-util";
 
 const query = gql`
@@ -27,61 +27,6 @@ const query = gql`
     }
   }
 `;
-
-/*
-async function iterateOverCursor(client, cursor, repository) {
-  const result = repository.split("/");
-  const options = { owner: result[0], name: result[1], after: cursor };
-
-  let myjson = await getGithubData(client, options, repositoryForks);
-  let myredis = await handlePromise(myjson);
-  await getCursorFromData(client, myredis, repository);
-}
-
-async function getCursorFromData(client, value, repository) {
-  let userCount = value.data.repository.forks.totalCount;
-  let edgeAry = value.data.repository.forks.edges;
-
-  processEdgeAry(edgeAry, repository);
-
-  let edgeAryLength = edgeAry.length;
-  let cursor = edgeAry[edgeAryLength - 1].cursor;
-  console.log("userCount = ", userCount);
-  console.log("edgeAry length = ", edgeAryLength);
-  console.log("cursor = ", cursor);
-
-  if (edgeAryLength < 100) {
-    return 1;
-  }
-
-  iterateOverCursor(client, cursor, repository);
-}
-
-function processEdgeAry(edgeAry, repository) {
-  edgeAry.forEach(function(item) {
-    let login = item.node.owner.login;
-    // eventually we will rename this method
-    // but for now it sadd's a member to a set
-    writeLocation(login, repository);
-  });
-}
-
-async function goGql(repository) {
-  let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
-  let client = await getClient(githubApiKey);
-  let myjson = await getInitialGithubData(client, repository, repositoryForks);
-  let myredis = await handlePromise(myjson);
-  console.log(myredis);
-  await getCursorFromData(client, myredis, repository);
-}
-
-const repositories = ["graphql/graphql-js"];
-
-repositories.forEach(function(repository) {
-  goGql(repository);
-});
-*/
-
 
 async function iterateOverCursor(client, cursor, options_in) {
   const options = {
@@ -114,7 +59,6 @@ async function getCursorFromData(client, options, value) {
 
   iterateOverCursor(client, cursor, options);
 }
-
 
 function processEdgeAry(edgeAry, repository) {
   edgeAry.forEach(function(item) {
