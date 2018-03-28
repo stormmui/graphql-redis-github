39f670f2b40ac4688b3ceb6298c8bf6d5b2aa633
Michael Angerman <stormasm@gmail.com>
2018-03-28 09:58:02 -0700

    completed renaming redis utils

--- src/redis/flushdb.js
+++ src/redis/flushdb.js
@@ -3,11 +3,8 @@ const { promisify } = require("util");
 
 export async function flushdb() {
   let client = redis.createClient();
-
   let flushdb = promisify(client.flushdb).bind(client);
   let result = await flushdb();
-
-  // console.log(`${readResult} has been saved.`);
   client.quit();
   return result;
 }
