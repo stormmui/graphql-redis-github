c233bda56dd0203df7fbb4bd3ba559dd84f47b89
Michael Angerman <stormasm@gmail.com>
2018-03-28 09:47:47 -0700

    mw

--- src/tests/redisUser.js
+++ src/tests/redisUser.js
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { describe, it } from "mocha";
 import { sismember, readLocation, readName } from "../redis/readUtils";
-import { sadd, writeName } from "../redis/writeUtils";
+import { sadd, hset } from "../redis/writeUtils";
 import { flushdb } from "../redis/flushdb";
 
 describe("Github Redis User", () => {
@@ -12,7 +12,7 @@ describe("Github Redis User", () => {
 
     it("Write location to redis", async () => {
       sadd("corvallis", "stormasm");
-      writeName("stormasm", "michael angerman");
+      hset("stormasm", "name", "michael angerman");
       const result1 = await sismember("corvallis", "stormasm");
       expect(result1).to.deep.equal(1);
       const result2 = await readName("stormasm");
