95b66d03ee7374c2dc5234676e0f9672d1518c2c
Michael Angerman <stormasm@gmail.com>
2018-03-27 20:45:54 -0700

    cc

--- src/repo/writeStarGazers.js
+++ src/repo/writeStarGazers.js
@@ -26,64 +26,6 @@ const query = gql`
   }
 `;
 
-/*
-async function iterateOverCursor(client, cursor, repository) {
-  const result = repository.split("/");
-  const options = { owner: result[0], name: result[1], after: cursor };
-
-  let myjson = await getGithubData(client, options, repositoryStarGazers);
-  let myredis = await handlePromise(myjson);
-  await getCursorFromData(client, myredis, repository);
-}
-
-async function getCursorFromData(client, value, repository) {
-  let userCount = value.data.repository.stargazers.totalCount;
-  let edgeAry = value.data.repository.stargazers.edges;
-
-  processEdgeAry(edgeAry, repository);
-
-  let edgeAryLength = edgeAry.length;
-  let cursor = edgeAry[edgeAryLength - 1].cursor;
-  console.log("userCount = ", userCount);
-  console.log("edgeAry length = ", edgeAryLength);
-  console.log("cursor = ", cursor);
-
-  if (edgeAryLength < 100) {
-    return 1;
-  }
-
-  iterateOverCursor(client, cursor, repository);
-}
-
-function processEdgeAry(edgeAry, repository) {
-  edgeAry.forEach(function(item) {
-    let login = item.node.login;
-    // eventually we will rename this method
-    // but for now it sadd's a member to a set
-    writeLocation(login, repository);
-  });
-}
-
-async function goGql(repository) {
-  let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
-  let client = await getClient(githubApiKey);
-  let myjson = await getInitialGithubData(
-    client,
-    repository,
-    repositoryStarGazers
-  );
-  let myredis = await handlePromise(myjson);
-  console.log(myredis);
-  await getCursorFromData(client, myredis, repository);
-}
-
-const repositories = ["graphql/graphql-js"];
-
-repositories.forEach(function(repository) {
-  goGql(repository);
-});
-*/
-
 async function iterateOverCursor(client, cursor, options_in) {
   const options = {
     repository: options_in.repository,
