ab2179322ee9874fa75f1f5f83596a666daa1081
Michael Angerman <stormasm@gmail.com>
2018-04-07 18:29:38 -0700

    cc

--- src/user/writeAvatar.js
+++ src/user/writeAvatar.js
@@ -18,21 +18,6 @@ const query = gql`
   }
 `;
 
-/*
-async function iterateOverCursor(client, cursor, options_in) {
-  const options = {
-    repository: options_in.repository,
-    login: options_in.login,
-    name: options_in.name,
-    after: cursor
-  };
-
-  let json = await getGithubData(client, options, query);
-  let data = await handlePromise(json);
-  await getCursorFromData(client, options, data);
-}
-*/
-
 async function getUserFromData(client, options, value) {
   let login = options.login;
   let avatar = value.data.user.avatarUrl;
@@ -47,52 +32,7 @@ async function getUserFromData(client, options, value) {
   hset(login, "avatar", avatar);
   hset(login, "location", location);
   hset(login, "name", name);
-  /*
-  processEdgeAry(edgeAry, options.repository);
-
-  let edgeAryLength = edgeAry.length;
-  let cursor = edgeAry[edgeAryLength - 1].cursor;
-  console.log("userCount = ", userCount);
-  console.log("edgeAry length = ", edgeAryLength);
-  console.log("cursor = ", cursor);
-
-  if (edgeAryLength < 100) {
-    return 1;
-  }
-
-  iterateOverCursor(client, cursor, options);
-*/
 }
-
-/*
-async function getCursorFromData(client, options, value) {
-  let userCount = value.data.repository.stargazers.totalCount;
-  let edgeAry = value.data.repository.stargazers.edges;
-
-  processEdgeAry(edgeAry, options.repository);
-
-  let edgeAryLength = edgeAry.length;
-  let cursor = edgeAry[edgeAryLength - 1].cursor;
-  console.log("userCount = ", userCount);
-  console.log("edgeAry length = ", edgeAryLength);
-  console.log("cursor = ", cursor);
-
-  if (edgeAryLength < 100) {
-    return 1;
-  }
-
-  iterateOverCursor(client, cursor, options);
-}
-
-function processEdgeAry(edgeAry, repository) {
-  edgeAry.forEach(function(item) {
-    let login = item.node.login;
-    // eventually we will rename this method
-    // but for now it sadd's a member to a set
-    sadd(repository, login);
-  });
-}
-*/
 
 async function goGql(options) {
   let githubApiKey = await getJsonKeyFromFile("./data/f1.js");
@@ -100,7 +40,6 @@ async function goGql(options) {
   let json = await getGithubData(client, options, query);
   let data = await handlePromise(json);
   await getUserFromData(client, options, data);
-  //await getCursorFromData(client, options, data);
 }
 
 async function writeAvatars(logins) {
