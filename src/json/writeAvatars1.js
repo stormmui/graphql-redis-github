f453a11277bda556e1314f4a42eedc9df608e7f0
Michael Angerman <stormasm@gmail.com>
2018-04-08 12:27:06 -0700

    mw

--- src/json/writeAvatars1.js
+++ src/json/writeAvatars1.js
@@ -2,41 +2,52 @@ import { hget, smembers } from "./../redis/readUtils";
 import { writeAvatar } from "./../user/writeAvatar";
 
 async function goGql2(login) {
-    let location = await hget(login,"location");
-    let name = await hget(login,"name");
-    let avatar = await hget(login, "avatar");
-    return new Promise((resolve, reject) => {
-      let myobj = {};
-      myobj.location = location;
-      myobj.name = name;
-      myobj.avatar = avatar;
-      //console.log(myobj);
-      resolve(myobj);
-      var reason = new Error('goGql2 problem');
-      reject(reason);
-    });
+  let location = await hget(login, "location");
+  let name = await hget(login, "name");
+  let avatar = await hget(login, "avatar");
+  return new Promise((resolve, reject) => {
+    let myobj = {};
+    myobj.location = location;
+    myobj.name = name;
+    myobj.avatar = avatar;
+    //console.log(myobj);
+    resolve(myobj);
+    var reason = new Error("goGql2 problem");
+    reject(reason);
+  });
 }
 
 async function goGql1(repository) {
   let logins = await smembers(repository);
   return new Promise((resolve, reject) => {
-  let myjson = [];
-  logins.forEach(function(login) {
-    let myobj = goGql2(login);
-    myjson.push(myobj);
-  });
-  resolve(myjson);
-  var reason = new Error('goGql1 problem');
-  reject(reason);
+    let myjson = [];
+    logins.forEach(function(login) {
+      let myobj = goGql2(login);
+      myjson.push(myobj);
+    });
+    resolve(myjson);
+    var reason = new Error("goGql1 problem");
+    reject(reason);
   });
 }
 
 async function writeAvatars(repositories) {
-  repositories.forEach(function(repository) {
-    let result = goGql1(repository);
-    console.log(result);
+  return new Promise((resolve, reject) => {
+    let result = null;
+    repositories.forEach(function(repository) {
+      result = goGql1(repository);
+      console.log(result);
+    });
+    resolve(result);
+    var reason = new Error("goGql1 problem");
+    reject(reason);
   });
 }
 
-const repositories = ["boundary/html5-node-diagram"];
-writeAvatars(repositories);
+async function go() {
+  const repositories = ["boundary/html5-node-diagram"];
+  let result = await writeAvatars(repositories);
+  console.log(result);
+}
+
+go();
