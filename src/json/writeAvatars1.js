c0b817b224391c5cf7bf163983f79e9e55064efe
Michael Angerman <stormasm@gmail.com>
2018-04-08 11:07:57 -0700

    mw

--- src/json/writeAvatars1.js
+++ src/json/writeAvatars1.js
@@ -1,24 +1,50 @@
 import { hget, smembers } from "./../redis/readUtils";
 import { writeAvatar } from "./../user/writeAvatar";
 
-async function goGql2(login,myjson) {
+async function goGql2(login) {
     let location = await hget(login,"location");
     let name = await hget(login,"name");
     let avatar = await hget(login, "avatar");
-    console.log(login, name, location, avatar);
-    let myobj = {};
-    myobj.location = location;
-    myobj.name = name;
-    myobj.avatar = avatar;
-    myjson.push(myobj);
-    return myjson;
+
+
+
+
+    return new Promise((resolve, reject) => {
+
+      //console.log(login, name, location, avatar);
+      let myobj = {};
+      myobj.location = location;
+      myobj.name = name;
+      myobj.avatar = avatar;
+      console.log(myobj);
+      resolve(myobj);
+
+      var reason = new Error('mom is not happy');
+      reject(reason);
+
+
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+
 }
 
 async function goGql1(repository) {
   let logins = await smembers(repository);
   let myjson = [];
   logins.forEach(function(login) {
-    let myjson = goGql2(login,myjson);
+    let myobj = goGql2(login);
+    myjson.push(myobj);
   });
 }
 
