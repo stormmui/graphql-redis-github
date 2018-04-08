d48541714e2dd555ea11f8e15ac8c32719cd9c01
Michael Angerman <stormasm@gmail.com>
2018-04-07 20:23:52 -0700

    starting to write out json

--- src/json/writeAvatars.js
+++ src/json/writeAvatars.js
@@ -1,15 +1,16 @@
 import { smembers } from "./../redis/readUtils";
-
+import { writeAvatars } from "./../user/writeAvatar";
 
 async function goGql(repository) {
   let result = await smembers(repository);
   console.log(result);
+  await writeAvatars(result);
 }
 
 async function writeUsers(repositories) {
   repositories.forEach(function(repository) {
-    goGql(repository)
-});
+    goGql(repository);
+  });
 }
 
 const repositories = ["boundary/html5-node-diagram"];
