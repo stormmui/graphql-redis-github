0e1903682eef0ec938aff15ede33719b9e9dda58
Michael Angerman <stormasm@gmail.com>
2018-04-09 13:01:56 -0700

    mw

--- src/json/writeAvatars.js
+++ src/json/writeAvatars.js
@@ -1,20 +1,9 @@
 import { hgetall, smembers } from "./../redis/readUtils";
 import { writeAvatar } from "./../user/writeAvatar";
 
-/*
-async function goGql2(users) {
-  users.forEach(function(user) {
-    let result = await hgetall(user);
-    console.log(result);
-  });
-}
-*/
-
 async function goGql1(repository) {
   let result = await smembers(repository);
-  // console.log(result);
   await writeAvatar(result);
-  // await goGql2(result);
 }
 
 async function writeAvatars(repositories) {
@@ -23,6 +12,10 @@ async function writeAvatars(repositories) {
   });
 }
 
-//const repositories = ["boundary/html5-node-diagram"];
-const repositories = ["augustl/nodejs-sandboxed-fs"];
+const repositories = [
+  "adamsanderson/ivy",
+  "augustl/nodejs-sandboxed-fs",
+  "boundary/html5-node-diagram"
+];
+
 writeAvatars(repositories);
