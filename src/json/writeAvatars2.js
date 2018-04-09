e0ba493288856c008e8a1fa49704c3d1f7d1e222
Michael Angerman <stormasm@gmail.com>
2018-04-08 19:09:04 -0700

    it appears to be working

---
+++ src/json/writeAvatars2.js
@@ -0,0 +1,43 @@
+import { hget, smembers } from "./../redis/readUtils";
+import { writeAvatar } from "./../user/writeAvatar";
+
+async function goGql2(login) {
+  let location = await hget(login, "location");
+  let name = await hget(login, "name");
+  let avatar = await hget(login, "avatar");
+  return new Promise((resolve, reject) => {
+    let myobj = {};
+    myobj.location = location;
+    myobj.name = name;
+    myobj.avatar = avatar;
+    //console.log(myobj);
+    resolve(myobj);
+    var reason = new Error("goGql2 problem");
+    reject(reason);
+  });
+}
+
+async function goGql1(repository) {
+  let logins = await smembers(repository);
+  let myjson = [];
+  for (const login of logins) {
+    // console.log(login);
+    let myobj = await goGql2(login);
+    myjson.push(myobj);
+    console.log(myjson);
+  }
+}
+
+async function writeAvatars(repositories) {
+  for (const repository of repositories) {
+    let result = await goGql1(repository);
+    // console.log(result);
+  }
+}
+
+async function go() {
+  const repositories = ["boundary/html5-node-diagram"];
+  await writeAvatars(repositories);
+}
+
+go();
